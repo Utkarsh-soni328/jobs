a1b3e6e2f664f190f7af065143a2ba85e228ed11
Utkarsh-soni328 <124017589+Utkarsh-soni328@users.noreply.github.com>
2023-09-15 14:04:35 +0530

    Add files via upload

---
+++ src/pages/desktop12.js
@@ -0,0 +1,195 @@
+const Desktop12 = () => {
+  return (
+    <div className="relative bg-white w-full h-[2083px] overflow-hidden text-left text-xl text-black font-inter">
+      <div className="absolute top-[0px] left-[0px] w-[1440px] h-[220px] text-center">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[220px]" />
+        <div className="absolute top-[77px] left-[43px] bg-gainsboro w-[291px] h-10" />
+        <div className="absolute top-[91px] left-[408px] w-[866px] h-[33px]">
+          <b className="absolute top-[6.29px] left-[584.99px] inline-block w-[143.24px] h-[26.71px]">
+            Financial Aids
+          </b>
+          <b className="absolute top-[3.14px] left-[358.65px] inline-block w-[103.88px] h-[26.71px]">
+            Lab
+          </b>
+          <b className="absolute top-[0px] left-[211.03px] inline-block w-[154.17px] h-[29.86px]">
+            Get Resources
+          </b>
+          <b className="absolute top-[0px] left-[107.16px] inline-block w-[103.88px] h-[26.71px]">
+            Learn
+          </b>
+          <b className="absolute top-[0px] left-[0px] inline-block w-[103.88px] h-[26.71px]">
+            About Us
+          </b>
+          <b className="absolute top-[3.14px] left-[450.49px] inline-block w-[117px] h-[26.71px]">
+            Job Portal
+          </b>
+          <b className="absolute top-[6.29px] left-[747.91px] inline-block w-[118.09px] h-[26.71px]">
+            Contact Us
+          </b>
+        </div>
+        <div className="absolute top-[165px] left-[1289px] text-base inline-block w-[141px] h-8">
+          Login/signup
+        </div>
+        <img
+          className="absolute h-[58.58%] w-[9.1%] top-[13.64%] right-[1.39%] bottom-[27.78%] left-[89.51%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/group.svg"
+        />
+      </div>
+      <div className="absolute top-[1616px] left-[0px] w-[1440px] h-[467px] text-3xl text-white">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[467px]" />
+        <div className="absolute top-[360px] left-[0px] bg-gray w-[1440px] h-[107px]" />
+        <div className="absolute top-[42px] left-[826px] w-[452px] h-[307px]">
+          <div className="absolute top-[50px] left-[35px] inline-block w-[417px] h-[104px]">
+            For any queries regarding the NPTEL website,availability of courses
+            or issues in accessing courses,please contact
+          </div>
+          <div className="absolute top-[0px] left-[0px] text-5xl text-center inline-block w-[194px] h-[38px]">
+            Contact Us:
+          </div>
+          <div className="absolute top-[168px] left-[28px] text-[inherit] inline-block w-[415px] h-[139px]">
+            {`NPTEL Administrator,IC & SR, 3rd floorIIT Madras, Chennai - 600036Tel : `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5905"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5905
+              </span>
+            </a>
+            {`, `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5908"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5908
+              </span>
+            </a>
+            {`, `}
+            <a className="text-[inherit]" href="tel:9363218521" target="_blank">
+              <span className="[text-decoration:underline]">9363218521</span>
+            </a>
+            {` (Mon-Fri 9am-6pm)Email : `}
+            <a
+              className="text-[inherit]"
+              href="mailto:support@nptel.iitm.ac.in"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                support@nptel.iitm.ac.in
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="absolute top-[42px] left-[162px] w-[194px] h-64 text-5xl">
+          <div className="absolute top-[0px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[218px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[173px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[128px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[83px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+        </div>
+        <b className="absolute top-[401px] left-[140px] text-xl inline-block text-center w-[317px] h-[25px]">
+          © - 2022 All rights reserved
+        </b>
+      </div>
+      <div className="absolute top-[377px] left-[82px] bg-lightskyblue w-[403px] h-[540px] hidden" />
+      <div className="absolute top-[275px] left-[43px] w-[1316px] h-[1250px] text-smi">
+        <div className="absolute top-[102px] left-[505px] w-[811px] h-[235px]">
+          <div className="absolute top-[0px] left-[0px] bg-lightskyblue w-[811px] h-[235px]" />
+          <div className="absolute top-[33px] left-[38px] bg-tomato w-[169px] h-[155px]" />
+          <div className="absolute top-[20px] left-[228px] text-13xl inline-block w-[149px] h-[39px]">
+            Title
+          </div>
+          <div className="absolute top-[66px] left-[228px] inline-block w-[470px] h-[82px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id
+            ipsum in eros gravida lobortis. Sed at ipsum convallis, pulvinar
+            lectus quis, lobortis tellus. Fusce varius id lectus vel luctus.
+            Fusce nec feugiat magna. Fusce lacinia non enim vitae imperdiet.
+            Cras posuere non mi quis mattis. Quisque nec volutpat nisi
+          </div>
+          <div className="absolute top-[169px] left-[228px] bg-tomato w-[121px] h-7" />
+          <b className="absolute top-[174px] left-[236px] inline-block text-white text-center w-[98px] h-4">
+            Apply
+          </b>
+        </div>
+        <div className="absolute top-[405px] left-[504px] w-[811px] h-[237px]">
+          <div className="absolute top-[0px] left-[0px] bg-lightskyblue w-[811px] h-[237px]" />
+          <div className="absolute top-[37px] left-[39px] bg-tomato w-[169px] h-[155px]" />
+          <div className="absolute top-[24px] left-[229px] text-13xl inline-block w-[149px] h-[39px]">
+            Title
+          </div>
+          <div className="absolute top-[70px] left-[229px] inline-block w-[470px] h-[82px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id
+            ipsum in eros gravida lobortis. Sed at ipsum convallis, pulvinar
+            lectus quis, lobortis tellus. Fusce varius id lectus vel luctus.
+            Fusce nec feugiat magna. Fusce lacinia non enim vitae imperdiet.
+            Cras posuere non mi quis mattis. Quisque nec volutpat nisi
+          </div>
+          <div className="absolute top-[173px] left-[229px] bg-tomato w-[121px] h-7" />
+          <b className="absolute top-[178px] left-[237px] inline-block text-white text-center w-[98px] h-4">
+            Apply
+          </b>
+        </div>
+        <div className="absolute top-[710px] left-[505px] w-[811px] h-[235px]">
+          <div className="absolute top-[0px] left-[0px] bg-lightskyblue w-[811px] h-[235px]" />
+          <div className="absolute top-[33px] left-[38px] bg-tomato w-[169px] h-[155px]" />
+          <div className="absolute top-[20px] left-[228px] text-13xl inline-block w-[149px] h-[39px]">
+            Title
+          </div>
+          <div className="absolute top-[66px] left-[228px] inline-block w-[470px] h-[82px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id
+            ipsum in eros gravida lobortis. Sed at ipsum convallis, pulvinar
+            lectus quis, lobortis tellus. Fusce varius id lectus vel luctus.
+            Fusce nec feugiat magna. Fusce lacinia non enim vitae imperdiet.
+            Cras posuere non mi quis mattis. Quisque nec volutpat nisi
+          </div>
+          <div className="absolute top-[169px] left-[228px] bg-tomato w-[121px] h-7" />
+          <b className="absolute top-[174px] left-[236px] inline-block text-white text-center w-[98px] h-4">
+            Apply
+          </b>
+        </div>
+        <div className="absolute top-[1013px] left-[504px] w-[811px] h-[237px]">
+          <div className="absolute top-[0px] left-[0px] bg-lightskyblue w-[811px] h-[237px]" />
+          <div className="absolute top-[37px] left-[39px] bg-tomato w-[169px] h-[155px]" />
+          <div className="absolute top-[24px] left-[229px] text-13xl inline-block w-[149px] h-[39px]">
+            Title
+          </div>
+          <div className="absolute top-[70px] left-[229px] inline-block w-[470px] h-[82px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id
+            ipsum in eros gravida lobortis. Sed at ipsum convallis, pulvinar
+            lectus quis, lobortis tellus. Fusce varius id lectus vel luctus.
+            Fusce nec feugiat magna. Fusce lacinia non enim vitae imperdiet.
+            Cras posuere non mi quis mattis. Quisque nec volutpat nisi
+          </div>
+          <div className="absolute top-[173px] left-[229px] bg-tomato w-[121px] h-7" />
+          <b className="absolute top-[178px] left-[237px] inline-block text-white text-center w-[98px] h-4">
+            Apply
+          </b>
+        </div>
+        <div className="absolute top-[0px] left-[39px] text-29xl text-darkslateblue-200 inline-block w-[668px] h-[120px]">
+          Apply for jobs
+        </div>
+        <img
+          className="absolute top-[429px] left-[0px] w-[500px] h-[500px] overflow-hidden"
+          alt=""
+          src="/profilingpana-1.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Desktop12;
